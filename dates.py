6bd6761fa5019742d9588a8b6563e0af0cf2908f
Jonathan Al <jonathanalemanquispe2@gmail.com>
2024-08-22 20:39:26 -0500

    commit base

---
+++ dates.py
@@ -0,0 +1,63 @@
+# list
+list = [1,3424, [2133], {1,3, "wsdasw"}, True, 0,2313, "jonaa.nw"]
+# print(list)
+
+# conjunto
+conjunto = {"esto", "es", "un", "conjunto"}
+# print(conjunto)
+
+# dictionary
+dicts = {
+    "name": "jona",
+    "age": 19,
+    "istPremium?": True
+}
+# print(dicts)
+
+# tuples
+tuples = ([123123],21343.21, "daww", {
+    "nombre": "adw",
+    "age": "119"
+})
+# print(tuples)
+
+# operadores
+
+op = 19 + 2
+less = op - 3
+div = 16 / 8
+
+div_baja = less // 8
+rest = 100 / 10
+
+print(op, less, div, div_baja, rest)
+
+# Ejercicio: Comparaciones Básicas
+
+# 1. Declara dos variables con valores numéricos
+a = 82139
+b = 292
+
+# 2. Usa operadores de comparación para verificar lo siguiente:
+
+# a) ¿Es 'a' mayor que 'b'?
+resultado1 = a > b
+
+# b) ¿Es 'a' igual a 'b'?
+resultado2 = a == b
+
+# c) ¿Es 'a' menor o igual a 'b'?
+resultado3 = a <= b
+
+# d) ¿'a' y 'b' son diferentes?
+resultado4 = a != b
+
+# e) ¿La suma de 'a' y 'b' es mayor que 30?
+resultado5 = (a + b) > 30
+
+# Imprime los resultados
+# print("a > b:", resultado1)
+# print("a == b:", resultado2)
+# print("a <= b:", resultado3)
+# print("a != b:", resultado4)
+# print("(a + b) > 30:", resultado5)
---
+++ if-else.py
@@ -0,0 +1,39 @@
+# imJona = 'yes! im jona'
+
+# if imJona:
+#     print("eres jona? " + imJona)
+# else: 
+#     print("no eres jona!")
+
+# edad = 15
+# if edad != 18 > 60:
+#     if edad > 40:
+#         print("tienes mas de", edad)
+#     print(edad, "ok... pasas")
+# elif 18 > edad > 12:
+#         print(edad, "tu no pasas!!!!")
+
+# else:
+#     print("nop... no pasas")
+
+# print("se ejecuta de todos modos")
+
+# ingreso_mensual = 28000
+# gasto_mensual = 7200
+
+# if ingreso_mensual > 20000:
+#     print(ingreso_mensual, "yep, ganas bien")
+
+#     if gasto_mensual >= ingreso_mensual:
+#         print("eh... gastas demasiado")
+#     elif gasto_mensual < ingreso_mensual:
+#         print("y no gastas mucho dinero")
+
+# elif gasto_mensual - ingreso_mensual > ingreso_mensual:
+#     print("gastas mas de lo que ganas en el mes.")
+
+# else:
+#     print("no ganas tanto como esperaba.")
+
+isNot = not 15 == 15
+print(isNot)
