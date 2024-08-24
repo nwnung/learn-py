068464c43a287d3ffc27e3ed579df93d2e5be9f5
Jonathan Al <jonathanalemanquispe2@gmail.com>
2024-08-23 19:33:57 -0500

    strings methods pt 1

---
+++ methods.py
@@ -0,0 +1,14 @@
+exm = "string by a method, "
+exm_two = 19
+exm3 = " hello jonaa "
+
+
+# methods from strings
+
+print(exm.strip())
+
+# print(dir(exm))
+print(exm.__add__("someone string"))
+
+print(exm.upper())
+print(exm.replace("s", "z"))
